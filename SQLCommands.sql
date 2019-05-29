0ba6841cbe6a41e818a189177b9989c16332b1a1
hiebelky <khiebel1001@gmail.com>
2019-05-28 19:22:55 -0700

    Added more users to database

--- SQLCommands.sql
+++ SQLCommands.sql
@@ -10,4 +10,8 @@ CREATE TABLE Applicant_User
 );
 
 INSERT INTO Applicant_User VALUES
-('Elise.Hebert', '1234', 'Elise', 'Hebert', 'heberte@oregonstate.edu');
+('Elise.Hebert', '1234', 'Elise', 'Hebert', 'heberte@oregonstate.edu'),
+('Kyle.Hiebel', '1234', 'Kyle', 'Hiebel', 'hiebelky@oregonstate.edu'),
+('Melody.Reebs', '1234', 'Melody', 'Reebs', 'reebsm@oregonstate.edu'),
+('Kevin.Ohrlund', '1234', 'Kevin', 'Ohrlund', 'ohrlundk@oregonstate.edu'),
+('Dougherty.Meghan', '1234', 'Meghan', 'Dougherty', 'dougherm@oregonstate.edu');

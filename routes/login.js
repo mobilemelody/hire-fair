24c7d8f872ab32d3993f643f408f7cb5fdab1b96
hiebelky <khiebel1001@gmail.com>
2019-05-28 18:41:11 -0700

    Added SQL

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+
 /* Route for login page */
 router.get('/', function(req, res, next) {
 	res.render('login');
@@ -9,8 +10,10 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
 	let context = {};
 	
+	var sql = "SELECT * FROM Applicant_User WHERE Username = ? AND Password = ?";
+	
 	// Verification of username and password
-
+	
 
 	// Create session
 

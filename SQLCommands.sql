e8e48d13b28cf076ca8765ac76d8963afc10fd45
hiebelky <khiebel1001@gmail.com>
2019-05-28 18:27:54 -0700

    Added SQL for created database

---
+++ SQLCommands.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Applicant_User
+(
+	Username VARCHAR(40) NOT NULL,
+	Password VARCHAR(40) NOT NULL,
+	First VARCHAR(40) NOT NULL,
+	Last VARCHAR(40) NOT NULL,
+	Email VARCHAR(100) NOT NULL,
+	PRIMARY KEY(Username),
+	UNIQUE(Username)
+);

252dda51df04bdaeb74cf95efcdea9a02fef4d3e
Meghan Dougherty <35378202+mrd5061@users.noreply.github.com>
2019-05-30 17:49:24 -0700

    Update SQLCommands.sql

--- SQLCommands.sql
+++ SQLCommands.sql
@@ -9,9 +9,26 @@ CREATE TABLE Applicant_User
 	UNIQUE(Username)
 );
 
+CREATE TABLE Employer_User
+(
+	Username VARCHAR(40) NOT NULL,
+	Password VARCHAR(40) NOT NULL,
+	First VARCHAR(40) NOT NULL,
+	Last VARCHAR(40) NOT NULL,
+	Email VARCHAR(100) NOT NULL,
+	CompanyName VARCHAR(40) NOT NULL,
+	Department VARCHAR(40),
+	Phone VARCHAR(20) NOT NULL,
+	PRIMARY KEY(Username),
+	UNIQUE(Username)
+);
+
 INSERT INTO Applicant_User VALUES
 ('Elise.Hebert', '1234', 'Elise', 'Hebert', 'heberte@oregonstate.edu'),
 ('Kyle.Hiebel', '1234', 'Kyle', 'Hiebel', 'hiebelky@oregonstate.edu'),
 ('Melody.Reebs', '1234', 'Melody', 'Reebs', 'reebsm@oregonstate.edu'),
 ('Kevin.Ohrlund', '1234', 'Kevin', 'Ohrlund', 'ohrlundk@oregonstate.edu'),
 ('Dougherty.Meghan', '1234', 'Meghan', 'Dougherty', 'dougherm@oregonstate.edu');
+
+INSERT INTO Employer_User VALUES
+('Elise.Hebert', '1234', 'Elise', 'Hebert', 'heberte@oregonstate.edu', 'OregonState', 'HR', '(555) 867-5309');

8257dca599451019748ed3b5c378476f94ee5717
hiebelky <khiebel1001@gmail.com>
2019-05-31 11:08:54 -0700

    Got rid of duplicate get function

--- routes/home.js
+++ routes/home.js
@@ -2,12 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 
-/* Route for home page */
-router.get('/', function(req, res, next) {
-	res.render('home');
-});
-
 router.post('/', function(req, res, next) {
+	// Check if the employer button was pressed
 	var employer = req.body.employer;
 	if(employer != undefined){
 		res.redirect('employer-login');

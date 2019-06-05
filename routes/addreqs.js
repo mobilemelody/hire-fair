39b89447eee89fddc3f2a66d81aa4fb6728529c6
Kyle Hiebel <hiebelky@oregonstate.edu>
2019-06-05 14:16:33 -0700

    Fixed crash for empty string

--- routes/addreqs.js
+++ routes/addreqs.js
@@ -8,17 +8,21 @@ router.get('/:id', function(req, res, next) {
 		
 		// Query to retrieve the listing that was just created
 		let mysql = req.app.get('mysql');
-		let sql = "SELECT * FROM listing WHERE listingID = ?";
+		let sql = "SELECT * FROM listing WHERE listingID='?'";
 		let inserts = [req.params.id];
 		mysql.pool.query(sql, inserts, function(error, results, fields) {
 			if(error) {
 				res.write(JSON.stringify(error));
 				res.end();
 			}
-			context['jobTitle'] = results['jobTitle'];
-			context['salary'] = results['salary'];
-			context['location'] = results['location'];
-			context['listingType'] = results['Internal'];
+			
+			if(results.length > 0){
+				context['jobTitle'] = results[0].jobTitle;
+				context['salary'] = results[0].salary;
+				context['location'] = results[0].location;
+				context['listingType'] = results[0].Internal;
+			}
+			
 			res.render('addreqs', context);
 		}) 
 	}

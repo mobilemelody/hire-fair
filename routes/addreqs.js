a46b5db27c233f13b5fcd4d630142231185096c4
mobilemelody <melody.reebs@gmail.com>
2019-06-06 18:52:25 -0700

    Functionality to add requirements to database

--- routes/addreqs.js
+++ routes/addreqs.js
@@ -17,6 +17,7 @@ router.get('/:id', function(req, res, next) {
 			}
 			
 			if(results != undefined){
+				context['listingid'] = req.params.id;
 				context['jobTitle'] = results[0].jobTitle;
 				context['salary'] = results[0].salary;
 				context['location'] = results[0].location;
@@ -26,13 +27,61 @@ router.get('/:id', function(req, res, next) {
 					context['listingType'] = "External";
 				}
 			}
-			
 			res.render('addreqs', context);
 		}) 
 	}
 	else {
 		res.render('employerlogin');
 	}
+});
+
+/* Route to add requirements to database */
+router.post('/:id', function(req, res, next) {
+	let context = {};
+
+	let mysql = req.app.get('mysql');
+	let sql = "INSERT INTO requirement (reqType, description, weight, listingID) VALUES ";
+	let inserts = [];
+
+	/* Add default education requirement */
+	sql += "(?, ?, ?, ?)";
+	inserts.push("education");
+	inserts.push(req.body['education-combo']);
+	inserts.push(req.body['education-slider']);
+	inserts.push(req.params.id);
+
+	/* Add default experience requirement */
+	sql += ", (?, ?, ?, ?)";
+	inserts.push("experience");
+	inserts.push(req.body['experience-combo']);
+	inserts.push(req.body['experience-slider']);
+	inserts.push(req.params.id);
+
+	/* Add custom requirements */
+	let reqTypes = ['education', 'experience', 'skills', 'certifications'];
+	for (let i = 0; i < 4; i++) {
+		let type = reqTypes[i];
+		let numReqs = req.body[type + '-num-requirements'];
+
+		for (let j = 0; j < numReqs; j++) {
+			sql += ", (?, ?, ?, ?)";
+			inserts.push(type);
+			inserts.push(req.body[type + '-custom-req-' + j]);
+			inserts.push(req.body[type + '-custom-req-' + j + '-slider']);
+			inserts.push(req.params.id);
+		}
+		
+	} 
+
+	mysql.pool.query(sql, inserts, function(error, results, fields) {
+		if(error) {
+			res.write(JSON.stringify(error));
+			res.end();
+		}
+					
+		res.redirect('/');
+	})
+
 });
 
 
